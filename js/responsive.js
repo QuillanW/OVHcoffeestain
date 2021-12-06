9ec6e177a936aead6509fa8cd2f198524a55d685
QuillanW <quillanwielhouwer@live.nl>
2021-12-06 09:50:04 +0100

    update

---
+++ js/responsive.js
@@ -0,0 +1,9 @@
+
+
+function vertaalNederlands(){
+    console.log("nerderlands")
+}
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'nl', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+  }
---
+++ js/sliders.js
@@ -0,0 +1,12 @@
+function slide1() {
+    document.getElementById('slider1').style.marginLeft = "0%"
+}
+function slide2() {
+    document.getElementById('slider1').style.marginLeft = "-50%"
+}
+function slide3() {
+    document.getElementById('slider1').style.marginLeft = "-100%"
+}
+function slide4() {
+    document.getElementById('slider1').style.marginLeft = "-150%"
+}
