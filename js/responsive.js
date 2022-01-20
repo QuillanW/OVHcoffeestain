09b356326f2031b10b303811bb3d10eef2fbd2ba
QuillanW <quillanwielhouwer@live.nl>
2022-01-20 16:53:05 +0100

    update

--- js/responsive.js
+++
@@ -1,9 +0,0 @@
-
-
-function vertaalNederlands(){
-    console.log("nerderlands")
-}
-
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'nl', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
-  }
--- js/sliders.js
+++ js/sliders.js
@@ -1,12 +1,12 @@
 function slide1() {
-    document.getElementById('slider1').style.marginLeft = "0%"
+    document.getElementById('slider1').style.marginLeft = "25%"
 }
 function slide2() {
-    document.getElementById('slider1').style.marginLeft = "-50%"
+    document.getElementById('slider1').style.marginLeft = "-25%"
 }
 function slide3() {
-    document.getElementById('slider1').style.marginLeft = "-100%"
+    document.getElementById('slider1').style.marginLeft = "-75%"
 }
 function slide4() {
-    document.getElementById('slider1').style.marginLeft = "-150%"
+    document.getElementById('slider1').style.marginLeft = "-125%"
 }
